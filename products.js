87308c49edf438c2c5766d582c9f2f667d80a287
edgardjustino <edgardpjustino@gmail.com>
2025-04-04 19:29:03 -0300

    making an object with promotions

---
+++ products.js
@@ -0,0 +1,90 @@
+const promocoes = {
+  bauducco: [
+    {
+      id: 1,
+      titulo: 'Bauducco Todo Dia',
+      link: 'https://www.bauduccotododia.com.br/',
+      image: 'images/promos/bauducco-todo-dia.jpg',
+      alt: 'imagem da promocao bauducco',
+    },
+  ],
+  danone: [
+    {
+      id: 2,
+      titulo: 'Danone Tudo de Bom',
+      link: 'https://www.promodanone.com.br/',
+      image: 'images/promos/danone-tudo-de-bom.jpg',
+      alt: 'imagem da promocao danone',
+    },
+  ],
+  nescau: [
+    {
+      id: 3,
+      titulo: 'Nescau Energia Para Jogar Junto',
+      link: 'https://www.nescau.com.br/promo',
+      image: 'images/promos/nescau-energia-jogar-junto.jpg',
+      alt: 'imagem da promocao nescau',
+    },
+  ],
+  brilhante: [
+    {
+      id: 4,
+      titulo: 'Brilhante comprou, brilhou, ganhou',
+      link: 'https://promobrilhante.com.br/home/',
+      image: 'images/promos/brilhante-comprou-brilhou-ganhou.jpg',
+      alt: 'imagem da promocao brilhante',
+      
+    }
+  ],
+};
+
+// let loja = {
+//   nome: "EletroShop",
+//     categorias: [
+//       {
+//         nome: "Eletrônicos",
+//         produtos: [
+//                 { id: 1,
+//                    nome: "Smartphone",
+//                    preco: 1999.90, estoque: 50
+//                 },
+
+//                 { id: 2,
+//                   nome: "Notebook",
+//                    preco: 4599.90,
+//                    estoque: 20 }
+//                 ]
+//       },
+//       {nome: "Móveis",
+//        produtos: [
+//         {
+//         id: 3,
+//         nome: "Sofá",
+//         preco: 1299.00,
+//         estoque: 15
+//       },
+//       { id: 4,
+//         nome: "Mesa",
+//         preco: 899.90,
+//         estoque: 30
+//       }
+//     ]
+//   }
+// ],
+//   clientes: [
+//         {
+//           id: 1,
+//           nome: "João",
+//           pedidos: [
+//             { id: 101,
+//               produtos: [1, 3],
+//               total: 3298.90 },
+//             { id: 102,
+//               produtos: [2],
+//               total: 4599.90
+//             }
+//           ]
+//         }
+//       ]
+//     };
+// Acessando dados profundamente aninhadosconsole.log(loja.categorias[0].produtos[0].nome); // "Smartphone"console.log(loja.clientes[0].pedidos[0].total); // 3298.90
